c15da643d91fb1f1a02ec558be0a450ca52c503e
Pio Pezzi <pio@email.unc.edu>
2021-11-13 02:43:47 -0500

    Worked on Player

--- fighter.py
+++ fighter.py
@@ -3,5 +3,18 @@ from projectile import Projectile
 import pygame as py
 import math
 from vector import Vector
-class Fighter:
-    """Squares placed by player to combat enemies."""
+
+class Player:
+    """Main player of the game."""
+    position: Vector
+    speed: float = 5.0
+    size: int = 1
+    social_points: int
+    time: int
+    toxicity: int
+
+    def move(self, new_position: Vector) -> None: 
+        vector: Vector = new_position - self.position 
+        unit_vector: Vector = vector.normalize()
+        speed_vector: Vector = unit_vector * self.speed
+        self.position = self.position + speed_vector 
